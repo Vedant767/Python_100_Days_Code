0b18ca0e2a07e697c3784000ee5707b1af181c68
Vedant <vedantgire48@gmail.com>
2022-01-12 09:38:08 +0530

    Day 16 OOPS/ Turtle/ PrettyTable

---
+++ Day_16/turtle_example.py
@@ -0,0 +1,14 @@
+import turtle
+
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.forward(200)
+timmy.right(90)
+timmy.forward(200)
+timmy.right(90)
+timmy.forward(200)
+timmy.right(90)
+timmy.forward(200)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
